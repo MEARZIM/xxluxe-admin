ccd2694cefddc488e1e29d137fdacbd0aea5dfb8
MEARZIM <asahaayan@gmail.com>
2024-07-13 12:37:43 +0530

    Add: prisma for database handeling

---
+++ app/(root)/page.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+
+export default function Home() {
+  const isOpen = useStoreModal((state) => state.isOpen);
+  const onOpen = useStoreModal((state) => state.onOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen])
+
+  return (
+    <>
+      <div className="font-medium">
+        Root Page
+      </div>
+    </>
+  );
+}
