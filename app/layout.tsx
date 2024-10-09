05d9ea5d5c17907fd79344f88bc8f9577d4f3223
MEARZIM <asahaayan@gmail.com>
2024-10-09 21:18:24 +0530

    chore: rename store to xxluxe-admin

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "XO Planet",
+  title: "xxluxe-admin",
   description: "Created by Ayan Saha",
 };
 
