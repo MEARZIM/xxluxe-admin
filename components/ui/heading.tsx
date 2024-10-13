0f133ef1f8a3a695a8c23692c284608756e390a6
MEARZIM <asahaayan@gmail.com>
2024-10-13 12:10:57 +0530

    style: heading style change

--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -13,7 +13,7 @@ const Heading = ({
     return (
         <>
             <div>
-                <h2 className="text-3xl text-bold tracking-tight">
+                <h2 className="text-3xl font-bold tracking-tight">
                     {title}
                 </h2>
                 <p className="text-sm text-muted-foreground">
